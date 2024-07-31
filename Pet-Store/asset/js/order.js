3964287e666863a3a242d95b789eb67debebe6e9
Phạm Đăng Quang <140483907+QUANGNOTFUN@users.noreply.github.com>
2024-07-31 14:18:27 +0700

    Update cart, parrot

---
+++ Pet-Store/asset/js/order.js
@@ -0,0 +1,18 @@
+// convet.js
+
+function addToCart(id, name, price) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || {};
+
+    if (cart[id]) {
+        cart[id].quantity += 1;
+    } else {
+        cart[id] = {
+            name: name,
+            price: price,
+            quantity: 1
+        };
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert('Đã thêm vào giỏ hàng');
+}
