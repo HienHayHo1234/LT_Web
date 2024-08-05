77841013e050e0df4fd9a8cedd6e32427c34a37a
Phạm Đăng Quang <140483907+QUANGNOTFUN@users.noreply.github.com>
2024-08-05 20:42:09 +0700

    Sửa trang admin

--- Pet-Store/asset/js/admin.js
+++ Pet-Store/asset/js/admin.js
@@ -33,3 +33,36 @@ document.getElementById('pet-form').addEventListener('submit', function(e) {
 //     localStorage.clear();
 // });
 
+function clickButtonAdd() {
+    document.getElementById('add-form').innerHTML = `
+        <a href="parrot.html" class="back-link">Quay Lại</a>
+        <h1>Thêm Sản Phẩm Mới</h1>
+        <form id="pet-form">
+            <div class="form-group">
+                <label for="pet-id">ID:</label>
+                <input type="text" id="pet-id" name="pet-id" required>
+            </div>
+            <div class="form-group">
+                <label for="pet-name">Tên:</label>
+                <input type="text" id="pet-name" name="pet-name" required>
+            </div>
+            <div class="form-group">
+                <label for="pet-price">Giá:</label>
+                <input type="number" id="pet-price" name="pet-price" required>
+            </div>
+            <div class="form-group">
+                <label for="pet-price-sale">Giá Khuyến Mãi:</label>
+                <input type="number" id="pet-price-sale" name="pet-price-sale" required>
+            </div>
+            <div class="form-group">
+                <label for="pet-quantity">Số Lượng:</label>
+                <input type="number" id="pet-quantity" name="pet-quantity" required>
+            </div>
+            <div class="form-group">
+                <label for="pet-image">URL Hình Ảnh:</label>
+                <input type="file" id="pet-image" name="pet-image" required>
+            </div>
+            <button type="submit">Thêm Sản Phẩm</button>
+        </form>
+    `;
+}
