1476111c35c3833d181bfa55e550c66eaf07f1b7
Phạm Đăng Quang <140483907+QUANGNOTFUN@users.noreply.github.com>
2024-08-11 19:12:25 +0700

    Thêm form đặt hàng

--- Pet-Store/pages/cat.php
+++ Pet-Store/pages/cat.php
@@ -1,9 +1,6 @@
 <?php
 // Khai báo các thông số kết nối cơ sở dữ liệu
-$host = "localhost";
-$dbname = "pet-store";
-$username = "root";
-$password = "";
+require '../config/config.php';
 
 try {
     // Tạo đối tượng PDO để kết nối với cơ sở dữ liệu MySQL
@@ -25,24 +22,24 @@ try {
 
 <div class="pets-grid">
     <?php if (!empty($pets)): ?>
-    <?php foreach ($pets as $pet): ?>
-    <div class="container-pets">
-        <img src="<?php echo htmlspecialchars($pet['urlImg']); ?>" alt="<?php echo htmlspecialchars($pet['name']); ?>">
-        <div class="row">
-            <p class="name-pet"><?php echo htmlspecialchars($pet['name']); ?></p>
-            <button class="heart" id="button1">❤</button>
-        </div>
-        <p class="text-price">Giá: <span class="price"><?php echo number_format($pet['price'], 0, ',', '.'); ?>đ</span>
-            ➱
-            <?php echo number_format($pet['priceSale'], 0, ',', '.'); ?>đ</p>
-        <button class="button view-detail" id="button2">Xem chi tiết</button>
-        <button class="button order" id="order-cat"
-            onclick="addToPet('<?php echo htmlspecialchars($pet['id'], ENT_QUOTES, 'UTF-8'); ?>')">
-            Thêm giỏ hàng
-        </button>
-    </div>
-    <?php endforeach; ?>
+        <?php foreach ($pets as $pet): ?>
+            <div class="container-pets">
+                <img src="<?php echo htmlspecialchars($pet['urlImg']); ?>" alt="<?php echo htmlspecialchars($pet['name']); ?>">
+                <div class="row">
+                    <p class="name-pet"><?php echo htmlspecialchars($pet['name']); ?></p>
+                    <button class="heart" id="button1">❤</button>
+                </div>
+                <p class="text-price">Giá: <span class="price"><?php echo number_format($pet['price'], 0, ',', '.'); ?>đ</span>
+                    ➱
+                    <?php echo number_format($pet['priceSale'], 0, ',', '.'); ?>đ</p>
+                <button class="button view-detail" id="button2">Xem chi tiết</button>
+                <button class="button order" id="order-cat"
+                    onclick="addToPet('<?php echo htmlspecialchars($pet['id'], ENT_QUOTES, 'UTF-8'); ?>')">
+                    Thêm giỏ hàng
+                </button>
+            </div>
+        <?php endforeach; ?>
     <?php else: ?>
-    <p>Chưa có sản phẩm nào.</p>
+        <p>Chưa có sản phẩm nào.</p>
     <?php endif; ?>
 </div>
--- Pet-Store/pages/dog.php
+++ Pet-Store/pages/dog.php
@@ -7,7 +7,7 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Thực hiện truy vấn để lấy tất cả các sản phẩm thuộc danh mục 'parrot'
+    // Thực hiện truy vấn để lấy tất cả các sản phẩm thuộc danh mục 'cat'
     $stmt = $conn->prepare("SELECT * FROM pets WHERE idLoai = :idLoai");
     $stmt->bindParam(':idLoai', $idLoai);
     $idLoai = 'dog'; // Danh mục cần lọc
@@ -20,26 +20,26 @@ try {
 }
 ?>
 
-
 <div class="pets-grid">
     <?php if (!empty($pets)): ?>
-    <?php foreach ($pets as $pet): ?>
-    <div class="container-pets">
-        <img src="<?php echo htmlspecialchars($pet['urlImg']); ?>" alt="<?php echo htmlspecialchars($pet['name']); ?>">
-        <div class="row">
-            <p class="name-pet"><?php echo htmlspecialchars($pet['name']); ?></p>
-            <button class="heart" id="button1">❤</button>
-        </div>
-        <p class="text-price">Giá: <span class="price"><?php echo number_format($pet['price'], 0, ',', '.'); ?>đ</span>
-            ➱
-            <?php echo number_format($pet['priceSale'], 0, ',', '.'); ?>đ</p>
-        <button class="button view-detail" id="button2">Xem chi tiết</button>
-        <button class="button order" id="button3"
-            onclick="addToCart('<?php echo htmlspecialchars($pet['id']); ?>', '<?php echo htmlspecialchars($pet['name']); ?>', <?php echo htmlspecialchars($pet['price']); ?>)">Đặt
-            hàng</button>
-    </div>
-    <?php endforeach; ?>
+        <?php foreach ($pets as $pet): ?>
+            <div class="container-pets">
+                <img src="<?php echo htmlspecialchars($pet['urlImg']); ?>" alt="<?php echo htmlspecialchars($pet['name']); ?>">
+                <div class="row">
+                    <p class="name-pet"><?php echo htmlspecialchars($pet['name']); ?></p>
+                    <button class="heart" id="button1">❤</button>
+                </div>
+                <p class="text-price">Giá: <span class="price"><?php echo number_format($pet['price'], 0, ',', '.'); ?>đ</span>
+                    ➱
+                    <?php echo number_format($pet['priceSale'], 0, ',', '.'); ?>đ</p>
+                <button class="button view-detail" id="button2">Xem chi tiết</button>
+                <button class="button order" id="order-cat"
+                    onclick="addToPet('<?php echo htmlspecialchars($pet['id'], ENT_QUOTES, 'UTF-8'); ?>')">
+                    Thêm giỏ hàng
+                </button>
+            </div>
+        <?php endforeach; ?>
     <?php else: ?>
-    <p>Chưa có sản phẩm nào.</p>
+        <p>Chưa có sản phẩm nào.</p>
     <?php endif; ?>
 </div>
